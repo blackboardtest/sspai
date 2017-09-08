868991ab0757860df5871f7f303c4a6a673ecb82
vimrus <vimrus.com@gmail.com>
2017-09-08 21:39:09 +0800

    * fix.

---
+++ index.js
@@ -0,0 +1,27 @@
+hubble.getXML('https://sspai.com/feed', function (error, response, $) {
+	$('item').each(function (index, value) {
+
+		var url = $(this).find('link').text();
+		var key = url.substring(url.lastIndexOf('/') + 1, url.lastIndexOf('.html'));
+		var dom = $(this);
+
+		articles.get('key', key, function (article) {
+			if (article) {
+				return;
+			}
+
+			var title = dom.find('title').text().trim();
+			var content = dom.find('description').text();
+			var summary = content.replace(/<\/?[^>]*>/g,'').trim().substring(0, 50);
+
+			var article = {
+				key: key,
+				title: title,
+				content: content,
+				summary: summary,
+				url: url
+			};
+			articles.append(article);
+		});
+	});
+});
