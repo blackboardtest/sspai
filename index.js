b5f88973c5c9c4f1298de771c442d4a26e44ce61
vimrus <vimrus.com@gmail.com>
2017-09-29 23:57:51 +0800

    * fix.

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 hubble.getXML('https://sspai.com/feed', function (error, response, $) {
-	$('item').each(function (index, value) {
+		$('item').each(function (index, value) {
 
-		var url = $(this).find('link').text();
-		var id = url.substring(url.lastIndexOf('/') + 1);
-		var dom = $(this);
+			var url = $(this).find('link').text();
+			var id = url.substring(url.lastIndexOf('/') + 1);
+			var dom = $(this);
 
-		articles.get('id', id, function (article) {
-			if (article) {
-				return;
-			}
+			articles.get('id', id, function (article) {
+				if (article) {
+					return;
+				}
 
-			var title = dom.find('title').text().trim();
-			var content = dom.find('description').text();
-			var image = dom.find('img').eq(0).attr('src');
+				var title = dom.find('title').text().trim();
+				var content = dom.find('description').text();
+				var $ = cheerio.load(content);
+				var image = $('img').eq(0).attr('src');
 
-			var article = {
-				id: id,
-				title: title,
-				content: content,
-				url: url,
-				image: image
-			};
-			articles.append(article);
+				var article = {
+					id: id,
+					title: title,
+					content: content,
+					url: url,
+					image: image
+				};
+				articles.append(article);
+			});
 		});
-	});
 });
