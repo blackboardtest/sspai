7845e9eba27524121d0e5aed36e0c222a15d6587
vimrus <vimrus.com@gmail.com>
2017-09-29 22:27:38 +0800

    * fix.

--- index.js
+++ index.js
@@ -12,13 +12,11 @@ hubble.getXML('https://sspai.com/feed', function (error, response, $) {
 
 			var title = dom.find('title').text().trim();
 			var content = dom.find('description').text();
-			var summary = content.replace(/<\/?[^>]*>/g,'').trim().substring(0, 50);
 
 			var article = {
 				id: id,
 				title: title,
 				content: content,
-				summary: summary,
 				url: url
 			};
 			articles.append(article);
